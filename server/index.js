7abff3652ec01205f1a3fc7d8b478c75066aea1f
Bibek Shrestha <shresthabbks@gmail.com>
2020-06-30 15:45:23 +0545

    s

--- server/index.js
+++ server/index.js
@@ -1,4 +1,10 @@
-const app = require("./app");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const auth = require("./router/authRouter");
+const details = require("./router/detailsRouter");
+const contact = require("./router/contactRouter");
+const path = require("path");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const port = process.env.PORT || 4000;
@@ -21,6 +27,19 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use("/admin", auth);
+app.use("/api", details);
+app.use("/contact", contact);
+app.use(express.static("../client/build"));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client", "build", "index.html")); // relative path
+});
 app.listen(port, () => {
   console.log(`server started on ${port}`);
 });
